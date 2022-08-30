616c0511321af99255849f017ab5106e0015306d
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2022-08-30 19:29:57 +0200

    Agregando TailWind

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-         <h1>Demo</h1>
-    </div>
+    <div className="bg-black p-5 text-center">
+       <h1 className="text-6xl font-mono text-white m-12">Marlon</h1>
+       <p className="text-white font-mono">Hello Marlon</p>
+     </div>
   );
 }
 
